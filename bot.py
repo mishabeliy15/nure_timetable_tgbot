417e7848db74924d19bdb393e8d1bad66b24fa24
Misha Beliy <misha.beliy15@gmail.com>
2018-08-29 02:49:07 +0300

    Change text

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['get'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить с параметрами')
+    bot.send_message(message.chat.id, 'Привет, скоро я буду парсить с параметрами(Test version)')
     update_photo()
     f = open("img.jpg","rb")
     bot.send_photo(message.chat.id,f)
