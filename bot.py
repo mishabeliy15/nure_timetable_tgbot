54dcd2ba9e7cd8a3e1a80a149918b8801c5c0835
Misha Beliy <misha.beliy15@gmail.com>
2018-08-29 18:32:14 +0300

    Big update. Added InlineKeyboardMarkup

--- bot.py
+++ bot.py
@@ -1,15 +1,46 @@
 import telebot
+from telebot import types
+
 from take_screenshot import *
 
-TOKEN = "652964125:AAFDvCnJGPueErD8rpyXEslwuql45zzZ6W4"
+TOKEN = ""
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['get'])
-def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, скоро я буду парсить с параметрами(Test version)')
-    update_with_cashe()
-    f = open(get_img_name(),"rb")
-    bot.send_photo(message.chat.id,f)
+groups_id =[6949772, 6949830, 6949706, 6949774, 7195531, 6949688, 6949724, 6949726, 6949654, 7195529, 7195533]
+
+def send_time_table(chat_id,group_id=groups_id[4]):
+    update_with_cashe(group_id)
+    f = open(get_img_name(group_id), "rb")
+    bot.send_photo(chat_id, f)
     f.close()
+
+@bot.message_handler(commands=['author'])
+def get_handler(message):
+    bot.send_message(message.chat.id, 'Author: Misha Beliy')
+
+@bot.message_handler(commands=['start','go'])
+def get_handler(message):
+    bot.send_message(message.chat.id, 'Привет. Напиши /get - для парса свежиго расписание ПЗПИ-18')
+    #send_time_table(message.chat.id)
+
+@bot.message_handler(commands=['get'])
+def test_handler(message):
+    keyboard = types.InlineKeyboardMarkup()
+    row =[]
+    for i in range(1,12):
+        callback_button = types.InlineKeyboardButton(text=str(i), callback_data=str(i-1))
+        row.append(callback_button)
+        if i==6 or i==11:
+            keyboard.row(*row)
+            row=[]
+
+    bot.send_message(message.chat.id, "Какой группы расписание Вы желаете?",reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    id_group = int(call.data)
+    if id_group>=0 and id_group<=11:
+        send_time_table(call.message.chat.id,group_id=groups_id[id_group])
 
 bot.polling()
